dfc0042ba00b0303f0b427742d5ed9973a239949
chaospredictor <kuzi81@gmail.com>
2017-09-25 10:27:19 +0300

    FlowerRecognizer/FlowerClassifer.mlmodel removed

--- FlowerRecognizer/ViewController.swift
+++ FlowerRecognizer/ViewController.swift
@@ -10,16 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var mainImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func cameraButton(_ sender: UIBarButtonItem) {
+        print("button pressed")
     }
-
-
+    
 }
 
